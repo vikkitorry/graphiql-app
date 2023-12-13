a078534d020b8e7e0840a825d5d86e21055fb08a
vikkitorry <vikkitorry6716@gmail.com>
2023-12-13 13:51:36 +0700

    refactor: footer, 'a' style

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import cls from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={cls.footer}>
+      <div className={cls.container}>
+        <a href="https://github.com/kotokatu">Natasha !!ADD PICTURE</a>
+        <a href="https://github.com/Kirich8">Maxim !!ADD PICTURE</a>
+        <a href="https://github.com/vikkitorry">Vika !!ADD PICTURE</a>
+      </div>
+      <div>2024</div>
+      <div>
+        <a href="https://rs.school/react/">ADD PICTURE</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
