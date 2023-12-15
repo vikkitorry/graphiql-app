a8bcb7017f11ed6dc058d2f972dee83d80fd8c7c
vikkitorry <vikkitorry6716@gmail.com>
2023-12-15 16:58:56 +0700

    feat: github logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,20 @@
 import cls from './Footer.module.scss';
 import RssIcon from '../../assets/RssIcon.svg';
+import GithubIcon from '../../assets/githubIcon.svg';
 
 const Footer = () => {
   return (
     <footer className={cls.footer}>
       <div className={cls.container}>
-        <a href="https://github.com/kotokatu">Natasha !!ADD PICTURE</a>
-        <a href="https://github.com/Kirich8">Maxim !!ADD PICTURE</a>
-        <a href="https://github.com/vikkitorry">Vika !!ADD PICTURE</a>
+        <a href="https://github.com/kotokatu">
+          <img src={GithubIcon} alt={'RSSshool icon'} width={40} height={40} />
+        </a>
+        <a href="https://github.com/Kirich8">
+          <img src={GithubIcon} alt={'RSSshool icon'} width={40} height={40} />
+        </a>
+        <a href="https://github.com/vikkitorry">
+          <img src={GithubIcon} alt={'RSSshool icon'} width={40} height={40} />
+        </a>
       </div>
       <div>2024</div>
       <div>
