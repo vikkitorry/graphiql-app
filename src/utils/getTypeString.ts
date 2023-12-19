10156334a4ab14d200ed0382f9e2bf57f75946ba
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-12-19 10:06:11 +0300

    feat: add nonnull and list types indicators

--- src/utils/getTypeString.ts
+++ src/utils/getTypeString.ts
@@ -1,12 +1,7 @@
 import { GraphQLType, isListType, isNonNullType, getNamedType } from 'graphql';
 
-//TODO: рекурсия!
-export const getTypeString = (type: GraphQLType) => {
-  if (isNonNullType(type) && isListType(type.ofType))
-    return `[${getNamedType(type).name}!]!`;
-  if (isListType(type) && isNonNullType(type.ofType))
-    return `[${getNamedType(type).name}]!`;
-  else if (isNonNullType(type)) return `${getNamedType(type).name}!`;
-  else if (isListType(type)) return `[${getNamedType(type).name}]`;
+export const getTypeString = (type: GraphQLType): string => {
+  if (isNonNullType(type)) return `${getTypeString(type.ofType)}!`;
+  if (isListType(type)) return `[${getTypeString(type.ofType)}]`;
   else return getNamedType(type).name;
 };
