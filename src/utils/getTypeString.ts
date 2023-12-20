1f2024aa76cc18f7da320b71caddcf91a4be2d2c
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-12-20 20:10:21 +0300

    refactor: refactor getTypeString

--- src/utils/getTypeString.ts
+++ src/utils/getTypeString.ts
@@ -3,5 +3,5 @@ import { GraphQLType, isListType, isNonNullType, getNamedType } from 'graphql';
 export const getTypeString = (type: GraphQLType): string => {
   if (isNonNullType(type)) return `${getTypeString(type.ofType)}!`;
   if (isListType(type)) return `[${getTypeString(type.ofType)}]`;
-  else return getNamedType(type).name;
+  return getNamedType(type).name;
 };
